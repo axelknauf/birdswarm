4a928760b756998959e3e5e3eac6ef48ecb7519e
Axel Knauf <axel.knauf@posteo.de>
2017-12-01 16:54:46 +0100

    Mainloop and Bird actors working, UI completely left aside

--- src/main/scala/de/axelknauf/birdswarm/ui/Main.scala
+++ src/main/scala/de/axelknauf/birdswarm/ui/Main.scala
@@ -1,19 +1,19 @@
 package de.axelknauf.birdswarm.ui
 
-import akka.actor.{ ActorSystem, Actor, ActorRef, Props, PoisonPill }
+import scala.concurrent.duration._
+import akka.actor.{ ActorSystem, Actor, ActorRef, Props, PoisonPill, Timers }
 import akka.event.Logging
 import language.postfixOps
 import scala.collection.mutable
 
 sealed trait Message
-// System startup and shutdown
-case class Bootstrap() extends Message
+// System shutdown
 case class Shutdown() extends Message
 // Change active/inactive state (if ticks are sent)
 case class Start() extends Message
 case class Stop() extends Message
 // Used by main loop and interaction
-case class Tick(millis: Long) extends Message
+case class Tick() extends Message
 case class NewBird(x: Int, y: Int) extends Message
 case class Position(x: Int, y: Int) extends Message
 
@@ -23,56 +23,55 @@ object Main extends App {
   val system = ActorSystem("birdswarm")
   val mainLoop = system.actorOf(MainLoop.props())
 
-  mainLoop ! Bootstrap
+  Thread.sleep(500)
   mainLoop ! Start
+  Thread.sleep(500)
+  mainLoop ! NewBird(1, 2)
+  Thread.sleep(500)
+  mainLoop ! NewBird(4, 2)
+  Thread.sleep(500)
+  mainLoop ! NewBird(3, 5)
+  Thread.sleep(500)
   mainLoop ! Stop
+  Thread.sleep(500)
   mainLoop ! Shutdown
 
   system.terminate()
 
 }
 
-class MainLoop extends Actor {
+class MainLoop extends Actor with Timers {
 
   import context._
 
   val log = Logging(context.system, this)
   var birds: mutable.Set[ActorRef] = mutable.Set()
 
-  // TODO how to extract common message behavior?
-  def commonBehavior: Receive = {
-    case Shutdown => context.stop(self)
-    case _ => log.info("Unknown message!")
-  }
-
-  def activeMainLoop: Receive = {
-    case NewBird(x, y) => birds.add(system.actorOf(Bird2.props(x, y)))
-    case Position(x, y) => log.info(s"Received position: ${x}, ${y}.")
-    case Stop => {
-      log.info("Mainloop stopping")
-      become(inactiveMainLoop)
-    }
-    case message => commonBehavior(message)
-  }
-
-  def inactiveMainLoop: Receive = {
-    case Start => {
-      // TODO add tick scheduling
-      log.info("Mainloop starting")
-      become(activeMainLoop)
-    }
-    case message => commonBehavior(message)
-  }
-
   def receive = {
-    case Bootstrap => {
-      log.info("Mainloop bootstrapping")
-      become(activeMainLoop)
-    }
+    // LIFECYCLE ----------------------------------------
     case Shutdown => {
-      log.info("Mainloop shutting down")
+      log.info("Shutting down")
       context.stop(self)
     }
+    case Start => {
+      log.info("Mainloop starting")
+      timers.startPeriodicTimer(Tick, Tick, 500.millis)
+    }
+    case Stop => {
+      log.info("Mainloop stopping")
+      timers.cancel(Tick)
+    }
+    // PROCESSING ----------------------------------------
+    case NewBird(x, y) => {
+      log.info(s"Creating new bird at ${x}, ${y}")
+      birds.add(system.actorOf(Bird.props(x, y)))
+    }
+    case Position(x, y) => log.info(s"Bird ${sender()} is at position: ${x}, ${y}.")
+    case Tick => {
+      log.info("Tick!")
+      birds.par.foreach(_ ! Tick)
+    }
+    case msg => log.info(s"Unknown message ${msg}!")
   }
 }
 
@@ -80,19 +79,22 @@ object MainLoop {
   def props(): Props = Props(classOf[MainLoop])
 }
 
-// TODO add ref to mainloop to bird in order to send position
-class Bird2(var x: Int, var y: Int) extends Actor {
+class Bird(var x: Int, var y: Int) extends Actor {
 
   val log = Logging(context.system, this)
 
   def receive = {
-    case Tick(millis) => {
-      log.info(s"reveived tick: ${millis}")
-      // sender() | Position(1, 2)
+    case Tick => {
+      calcNewPosition()
+      sender() ! Position(x, y)
     }
+  }
+
+  def calcNewPosition() {
+    log.info("Calculating new position (NOOP).")
   }
 }
 
-object Bird2 {
-  def props(x: Int, y: Int): Props = Props(classOf[Bird2], x, y)
+object Bird {
+  def props(x: Int, y: Int): Props = Props(classOf[Bird], x, y)
 }
